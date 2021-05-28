ac3cb712f7cc01c264348d521ea02a00ba5eccf2
Rizqi Imam Gilang Widianto <widiantorizqi@gmail.com>
2021-05-28 10:28:10 +0700

    initial commit to remote repo

---
+++ MakeFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MakeFood
+//
+//  Created by Rizqi Imam on 28/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
